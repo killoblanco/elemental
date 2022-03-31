53d1c5d051d2de0141efaba9f0d17ae39f486865
Kamilo Vasquez <killo.blanco@gmail.com>
2022-03-30 20:41:49 -0500

    Create stack.tsx

---
+++ src/lib/layout/stack.tsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
+import { CSSProperties } from 'react';
+import { BaseElement } from '../elemental';
+import { withSX } from '../utils/with';
+
+export interface StackProps extends BaseElement<'div'> {
+  gap?: CSSProperties['gap'];
+  dir?: CSSProperties['flexDirection'];
+  alignX?: CSSProperties['justifyContent'];
+  alignY?: CSSProperties['alignItems'];
+  growItems?: boolean;
+}
+
+export const Stack = styled(motion.div)<StackProps>(({ theme, sx, ...props }) => ({
+  ...withSX(theme, sx),
+  display: 'flex',
+  justifyContent: props.alignX,
+  alignItems: props.alignY,
+  gap: props.gap && typeof props.gap === 'number' ? theme.spacing(props.gap) : props.gap,
+  flexDirection: props.dir,
+  ...(props.growItems && { '& > *': { flexGrow: 1 } }),
+}));
